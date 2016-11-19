197c2e0eae1d06f1866739c3e17ba817cc308998
bz_benzait@esi.dz <Zaki@BENZAIT1993>
2016-11-19 08:42:38 +0100

    Mettre la classe AbstractFile une interface

--- AbstractFactory/src/fr/paris10/miage/file/AbstractFile.java
+++ AbstractFactory/src/fr/paris10/miage/file/AbstractFile.java
@@ -1,5 +1,32 @@
 package fr.paris10.miage.file;
 
-public abstract class AbstractFile {
+
+public interface  AbstractFile {
+	public enum OpenMode {
+        READ(), WRITE(), APPEND();
+
+        private OpenMode() {
+        }
+    }
+
+ abstract int  getId();
+
+ abstract  String getName();
+
+ //abstract  UserRegistry.User getUser();
+
+ abstract   OpenMode getMode();
+
+ abstract   boolean open(OpenMode mode);
+
+ abstract   boolean close();
+
+ abstract   void rename(String name);
+
+ abstract  String read();
+
+ abstract  boolean write(String content);
+
+ abstract  int size();
 
 }
